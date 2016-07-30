7dab333e16b13d95e7d7a1ab72f175b18d358a8c
JaneWang <janewang08@gmail.com>
2016-07-30 16:31:03 +0300

    Jane's R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invers <- matrix(nrow = dim(x)[1], ncol = dim(x)[2])
+    matequal <- function(x, y){
+        is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
+    }
+    set <- function(y) {
+        if (! matequal(x,y)){
+            x<<- y
+            invers <<- matrix(nrow = dim(y)[1], ncol = dim(y)[2])
+        }
+    }
+    get <- function() x
+    setinverse <- function(inv) invers <<- inv
+    getinverse <- function() invers
+    list(set=set, get=get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -12,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        
+        if (all(is.na(m))){
+            invers <- solve(x$get())
+            x$setinverse(invers)
+            invers
+        }
+        else {
+            m
+        }        
 }
